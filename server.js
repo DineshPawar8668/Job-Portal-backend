2769ad3886089cdf945180ad8f9e2b50bc938a21
Dinesh Pawar <131751260+DineshPawar8668@users.noreply.github.com>
2025-05-24 10:09:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,19 +3,21 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const cors = require('cors');
 
- const corsOptions = {
-   origin: 'https://job-portal-frontend-bay-psi.vercel.app/api'
- //  origin: 'https://job-portal-frontend-86ph.onrender.com/api',
-   credentials: true,
- };
-
 dotenv.config();
 connectDB();
 
 const app = express();
+
+// COrs Configuration
+const corsOptions = {
+  origin: 'https://job-portal-frontend-bay-psi.vercel.app',
+  credentials: true
+};
+
 app.use(cors(corsOptions));
 app.use(express.json());
 
+// Routes
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/jobs', require('./routes/jobRoutes'));
 app.use('/api/applications', require('./routes/applicationRoutes'));
