9d8d6b127a8eb6dcfaf1c4b942f5eaceac581178
Dinesh Pawar <131751260+DineshPawar8668@users.noreply.github.com>
2025-05-24 08:41:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const connectDB = require('./config/db');
 const cors = require('cors');
 
 const corsOptions = {
-  origin: 'https://job-portal-frontend-bay-psi.vercel.app/api',
+  // origin: 'https://job-portal-frontend-bay-psi.vercel.app/api'
+  origin: 'https://job-portal-frontend-86ph.onrender.com/',
   credentials: true,
 };
 
